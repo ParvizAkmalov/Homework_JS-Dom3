c67af6888a2c08db566bb3129c85c90786c069a2
Parviz_8901 <parvizakmalov39@gmail.com>
2024-04-11 12:26:08 +0500

    Leeson jc homework dom || 3

---
+++ main.js
@@ -0,0 +1,76 @@
+let MinusKatta = document.querySelector(".conPlusMinus1")
+let MinusKatta2 = document.querySelector(".conPlusMinus22")
+let  uzgaruvchi = document.querySelector(".ConLeftH11")
+let uzgaruvchi2 = document.querySelector(".ConLeftH12")
+let Lettopcd2 = document.querySelector(".Lettopcd2")
+let lefttopcomponendDiv2btn = document.querySelector(".lefttopcomponendDiv2btn")
+let lefttopcomponendDiv2btn2 = document.querySelector(".lefttopcomponendDiv2btn2")
+let ltcdikki = document.querySelector(".ltcdikki")
+let lbcd2ikki = document.querySelector(".lbcd2ikki")
+let lbcd2ikki2 = document.querySelector(".lbcd2ikki2")
+let rbcdikki2 = document.querySelector(".rbcdikki2")
+let rbcdikki2ikki = document.querySelector(".rbcdikki2ikki")
+let rbcdikki2ikki2 = document.querySelector(".rbcdikki2ikki2")
+let rbcdikk2 = document.querySelector(".rbcdikk2")
+let rbcdikk22 = document.querySelector(".rbcdikk22")
+let rbcdikk23 = document.querySelector(".rbcdikk23")
+
+rbcdikk22.onclick = () => {
+    rbcdikk2.innerHTML++
+}
+rbcdikk23.onclick = () => {
+    if(rbcdikk2.innerHTML == 0){
+     rbcdikk2.innerHTML = 0
+    }else{
+     rbcdikk2.innerHTML--
+    }
+  }
+
+rbcdikki2ikki.onclick = () => {
+    rbcdikki2.innerHTML++
+}
+rbcdikki2ikki2.onclick = () => {
+    if(rbcdikki2.innerHTML == 0){
+     rbcdikki2.innerHTML = 0
+    }else{
+     rbcdikki2.innerHTML--
+    }
+  }
+
+lbcd2ikki.onclick = () => {
+    ltcdikki.innerHTML++
+}
+lbcd2ikki2.onclick = () => {
+    if(ltcdikki.innerHTML == 0){
+     ltcdikki.innerHTML = 0
+    }else{
+     ltcdikki.innerHTML--
+    }
+  }
+
+lefttopcomponendDiv2btn.onclick = () => {
+    Lettopcd2.innerHTML++
+ }
+ lefttopcomponendDiv2btn2.onclick = () => {
+    if(Lettopcd2.innerHTML == 0){
+     Lettopcd2.innerHTML = 0
+    }else{
+     Lettopcd2.innerHTML--
+    }
+  }
+MinusKatta.onclick = () => {
+   uzgaruvchi.innerHTML++
+   uzgaruvchi2.innerHTML++
+}
+MinusKatta2.onclick = () => {
+   if(uzgaruvchi.innerHTML == 1){
+    uzgaruvchi.innerHTML = 1
+   }else{
+    uzgaruvchi.innerHTML--
+   }
+   if(uzgaruvchi2.innerHTML == 2){
+    uzgaruvchi2.innerHTML = 2
+   }else{
+    uzgaruvchi2.innerHTML--
+   }
+ }
